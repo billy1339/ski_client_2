16876a3caa9e56735f4da274a75178ea0a2ac9ed
Billy1339 <william.m.sargent@gmail.com>
2014-12-12 17:22:05 -0500

    maps data working in template, still need image

--- js/controllers/map.controller.js
+++ js/controllers/map.controller.js
@@ -1,19 +1,37 @@
 angular.module('Ski').controller('MapCtrl', function($scope, $http) {
   'use strict';
-  $scope.name = "William"
-
-  $http.$.get('/path/to/file', function(data) {
-    /*optional stuff to do after success */
-  /});
-
-  var mapOptions = {
-    center: new google.maps.LatLng(43.6121339, -72.8010977),
-    zoom: 14
-  };
-
-  $scope.map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
 
 
+  $http.get('https://quiet-journey-8066.herokuapp.com/mountains/5').success(function(response) {
+      console.log(response);
+      $scope.mountain = response.name;
+      $scope.lat = response.latitude;
+      $scope.lng = response.longitude;
+    });
 
 });
+
+angular.module('Ski').directive('mapCanvas', function() {
+  return {
+    link: function(scope, element) {
+      var mapOptions = {
+        center: { lat: -34.397, lng: 150.644},
+        zoom: 8
+      };
+      var map = new google.maps.Map(element[0], mapOptions);
+    }
+  };
+});
+
+
+// <script type="text/javascript">
+//         function initialize() {
+//           var mapOptions = {
+//             center: { lat: -34.397, lng: 150.644},
+//             zoom: 8
+//           };
+//           var map = new google.maps.Map(document.getElementById('map-canvas'),
+//               mapOptions);
+//         }
+
+//   </script>
