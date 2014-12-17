5ac935af5e3c6f3608c91999eaec1886306a3a02
Billy1339 <william.m.sargent@gmail.com>
2014-12-17 17:29:21 -0500

    new inputs save a specific url

--- js/controllers/map.controller.js
+++ js/controllers/map.controller.js
@@ -1,16 +1,7 @@
 angular.module('Ski').controller('MapCtrl', function($scope, $http, MountainFactory) {
   'use strict';
 
-  $scope.imageCategories = {
-    lift: {title: 'Lift', url: 'http://i.imgur.com/R94MVUJ.png'},
-    parking: {parking: 'Parking', url: 'http://i.imgur.com/ABBZTQV.png'},
-    trailClosed: {trailClosed: 'Trail Closed', url: 'http://i.imgur.com/rmRJKuY.png'},
-    trailConditions: {trailConditions: 'Trail Conditions', url:'http://i.imgur.com/UYs1CFk.png'},
-    powder: {title: 'Powder', url: 'http://i.imgur.com/hel1qm8.png'},
-    lines: {title: 'Long Lines', url: 'http://i.imgur.com/7gawLsF.png'},
-    lodge: {title: 'Lodge', url: 'http://i.imgur.com/Xz8JpuJ.png'},
-    park: {title: 'Snow Park', url: 'http://i.imgur.com/A9AQssm.png'}
 
-  };
+
 });
 
--- js/controllers/marker.controller.js
+++ js/controllers/marker.controller.js
@@ -1,16 +1,27 @@
 angular.module('Ski').controller('MarkerCtrl', function($scope, $http, $q) {
   'use strict';
 
-  $scope.allCategories = {
-    lift: {title: 'Lift', url: 'images/cablecar.png'},
-    parking: {parking: 'Parking', url: ''},
-    trail: {trail: 'Trail Conditions', url: ''},
-    snow: {title: 'Snow Conditions', url: 'images/snowy-2.png'},
-    lines: {title: 'Long Lines', url: ''},
-    lodge: {title: 'Lodge Status', url: ''},
-    park: {title: 'Park', url: ''}
-
+  $scope.imageCategories = {
+    'lift': {title: 'Lift', url: 'http://i.imgur.com/R94MVUJ.png'},
+    'parking': {title: 'Parking', url: 'http://i.imgur.com/ABBZTQV.png'},
+    'trailClosed': {title: 'Trail Closed', url: 'http://i.imgur.com/rmRJKuY.png'},
+    'trailConditions': {title: 'Trail Conditions', url:'http://i.imgur.com/UYs1CFk.png'},
+    'powder': {title: 'Powder', url: 'http://i.imgur.com/hel1qm8.png'},
+    'lines': {title: 'Long Lines', url: 'http://i.imgur.com/7gawLsF.png'},
+    'lodge': {title: 'Lodge', url: 'http://i.imgur.com/Xz8JpuJ.png'},
+    'park': {title: 'Snow Park', url: 'http://i.imgur.com/A9AQssm.png'}
   };
+  // to match, possible downcase and to symbol?
+  $scope.arrayOfCategories = [
+    'Lift',
+    'Parking',
+    'Trail Closed',
+    'Trail Conditions',
+    'Powder',
+    'Long Lines',
+    'Lodge',
+    'Snow Park',
+  ];
 
   $scope.makeMarker = function() {
     var myLatlng = setInitialLatLong();
@@ -47,12 +58,20 @@ angular.module('Ski').controller('MarkerCtrl', function($scope, $http, $q) {
     });
 
   };
+  var findInputUrl = function(input) {
+    var lower, inputObject;
+    lower = input.toLowerCase();
+    inputObject = $scope.imageCategories[lower].url;
+    return inputObject;
 
+  };
 
   $scope.submitMarker = function(input, description) {
+    var url, params;
     $scope.marker.draggable = false;
-    var params = {
-      input: {category: input.category, longitude:$scope.marker.position.D, latitude: $scope.marker.position.k, mountain_id: $scope.$$prevSibling.mountain.id,  }
+    url = findInputUrl(input.category);
+    params = {
+      input: {category: input.category, url: url, longitude:$scope.marker.position.D, latitude: $scope.marker.position.k, mountain_id: $scope.$$prevSibling.mountain.id,  }
     };
     $http.post('https://quiet-journey-8066.herokuapp.com/inputs', JSON.stringify(params)).success(function(response){
       $q.all(submitDescription(description, response.id)).then(function() {
