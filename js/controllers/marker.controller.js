c45015d875c4dd0f1508d6ae8204a92cdf763ff5
Billy1339 <william.m.sargent@gmail.com>
2014-12-18 12:54:33 -0500

    form and able to create new descriptions in the modals

--- js/controllers/marker.controller.js
+++ js/controllers/marker.controller.js
@@ -4,23 +4,23 @@ angular.module('Ski').controller('MarkerCtrl', function($scope, $http, $q) {
   $scope.imageCategories = {
     'lift': {title: 'Lift', url: 'http://i.imgur.com/R94MVUJ.png'},
     'parking': {title: 'Parking', url: 'http://i.imgur.com/ABBZTQV.png'},
-    'trailClosed': {title: 'Trail Closed', url: 'http://i.imgur.com/rmRJKuY.png'},
-    'trailConditions': {title: 'Trail Conditions', url:'http://i.imgur.com/UYs1CFk.png'},
+    'trail-closed': {title: 'Trail-Closed', url: 'http://i.imgur.com/rmRJKuY.png'},
+    'trail-conditions': {title: 'Trail-Conditions', url:'http://i.imgur.com/UYs1CFk.png'},
     'powder': {title: 'Powder', url: 'http://i.imgur.com/hel1qm8.png'},
-    'lines': {title: 'Long Lines', url: 'http://i.imgur.com/7gawLsF.png'},
+    'lines': {title: 'Long-Lines', url: 'http://i.imgur.com/7gawLsF.png'},
     'lodge': {title: 'Lodge', url: 'http://i.imgur.com/Xz8JpuJ.png'},
-    'park': {title: 'Snow Park', url: 'http://i.imgur.com/A9AQssm.png'}
+    'snow-park': {title: 'Snow-Park', url: 'http://i.imgur.com/A9AQssm.png'}
   };
 
   $scope.arrayOfCategories = [
     'Lift',
     'Parking',
-    'Trail Closed',
-    'Trail Conditions',
+    'Trail-Closed',
+    'Trail-Conditions',
     'Powder',
-    'Long Lines',
+    'Long-Lines',
     'Lodge',
-    'Snow Park',
+    'Snow-Park',
   ];
 
   $scope.makeMarker = function() {
@@ -73,17 +73,28 @@ angular.module('Ski').controller('MarkerCtrl', function($scope, $http, $q) {
     params = {
       input: {category: input.category, url: url, longitude:$scope.marker.position.D, latitude: $scope.marker.position.k, mountain_id: $scope.$$prevSibling.mountain.id,  }
     };
-    $http.post('https://quiet-journey-8066.herokuapp.com/inputs', JSON.stringify(params)).success(function(response){
+    $http.post('https://quiet-journey-8066.herokuapp.com/inputs', params).success(function(response){
       $q.all(submitDescription(description, response.id)).then(function() {
         clearForm();
       });
     });
+  };
 
+  $scope.newDescription = function(description, id) {
+    var params = {
+      description: {body: description.new, input_id: id}
+    };
+    $q.all($http.post('https://quiet-journey-8066.herokuapp.com/descriptions', params).success(function() {
+    }).then(function() {
+      $scope.description = {};
+    }));
   };
 
 });
 
-angular.module('Ski').directive('gaModal', function() {
+
+
+angular.module('Ski').directive('wsDescription', function() {
     return {
         restrict: 'E',
 
