6a612e1c3e50f06ef3969c85672457bb19db7a8d
Billy1339 <william.m.sargent@gmail.com>
2014-12-19 02:13:18 -0500

    geo location working

--- js/controllers/header.controller.js
+++ js/controllers/header.controller.js
@@ -4,5 +4,27 @@ angular.module('Ski').controller('HeaderCtrl', function($scope, $http, MountainF
     $rootScope.mtnId = number;
   };
 
+  $scope.getGeoPosition = function() {
+    // if (geoPosition.init()) {
+    //    geoPosition.getCurrentPosition(geoSuccess, geoError);
+    // }
+
+
+    if(geoPosition.init()){  // Geolocation Initialisation
+            geoPosition.getCurrentPosition(success_callback,error_callback,{enableHighAccuracy:true});
+    } else{
+          alert('you didnt give me permission')
+    }
+    function success_callback(p){
+        alert('latitude is: '+p.coords.latitude+' longitude is: '+p.coords.longitude);
+        // p.latitude : latitude value
+        // p.longitude : longitude value
+    }
+
+    function error_callback(p){
+        // p.message : error message
+        alert(p.message);
+    }
+  };
 
 });
